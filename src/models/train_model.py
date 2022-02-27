8c937d91ea53f1b075c0859c0ed1b7ccbf1088f5
u-masao <4973920+u-masao@users.noreply.github.com>
2022-02-27 21:54:11 +0900

    コマンドライン引数を有効化。Train, Test を分割。

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,6 @@
 import click
 import mlflow
+import numpy as np
 import optuna
 import sklearn.datasets
 import sklearn.ensemble
@@ -7,41 +8,67 @@ import sklearn.model_selection
 import sklearn.svm
 
 
-def objective(trial):
-    mlflow.start_run()
-    iris = sklearn.datasets.load_iris()
-    x, y = iris.data, iris.target
+def objective_wrapper(x, y, **kwargs):
+    def objective(trial):
+        mlflow.start_run(run_name=kwargs["mlflow_run_name"])
 
-    classifier_name = trial.suggest_categorical(
-        "classifier", ["SVC", "RandomForest"]
-    )
-    mlflow.log_param("classifier", classifier_name)
-    if classifier_name == "SVC":
-        svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
-        classifier_obj = sklearn.svm.SVC(C=svc_c, gamma="auto")
-        mlflow.log_param("svc_c", svc_c)
-    else:
-        rf_max_depth = trial.suggest_int("rf_max_depth", 2, 32, log=True)
-        classifier_obj = sklearn.ensemble.RandomForestClassifier(
-            max_depth=rf_max_depth, n_estimators=10
+        classifier_name = trial.suggest_categorical(
+            "classifier", ["SVC", "RandomForest"]
         )
-        mlflow.log_param("rf_max_depth", rf_max_depth)
+        mlflow_params = {}
+        mlflow_params["classifier"] = classifier_name
 
-    score = sklearn.model_selection.cross_val_score(
-        classifier_obj, x, y, n_jobs=-1, cv=3
-    )
-    accuracy = score.mean()
-    mlflow.log_metric("avg_accuracy", accuracy)
+        if classifier_name == "SVC":
+            svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
+            classifier_obj = sklearn.svm.SVC(C=svc_c, gamma="auto")
+            mlflow_params["svc_c"] = svc_c
+        else:
+            rf_max_depth = trial.suggest_int("rf_max_depth", 2, 32, log=True)
+            classifier_obj = sklearn.ensemble.RandomForestClassifier(
+                max_depth=rf_max_depth, n_estimators=10
+            )
+            mlflow_params["rf_max_depth"] = rf_max_depth
 
-    mlflow.end_run()
-    return accuracy
+        scores = sklearn.model_selection.cross_validate(
+            classifier_obj,
+            x,
+            y,
+            cv=3,
+            n_jobs=-1,
+            scoring=("accuracy"),
+        )
+        accuracy = np.mean(scores["test_score"])
+
+        mlflow.log_metric("avg_accuracy", accuracy)
+        mlflow.log_params(mlflow_params)
+        mlflow.end_run()
+        return accuracy
+
+    return objective
 
 
 @click.command()
 @click.option("--mlflow-run-name", type=str, default="mlproject_template")
+@click.option("--seed-train-test-split", type=int, default=1234)
 def main(**kwargs):
+    iris = sklearn.datasets.load_iris()
+    x, y = iris.data, iris.target
+    (
+        train_x,
+        test_x,
+        train_y,
+        test_y,
+    ) = sklearn.model_selection.train_test_split(
+        x,
+        y,
+        test_size=0.30,
+        random_state=kwargs["seed_train_test_split"],
+        shuffle=True,
+        stratify=y,
+    )
+
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective_wrapper(train_x, train_y, **kwargs), n_trials=100)
     print(study.best_trial)
 
 
